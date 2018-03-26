8a30be7a59a68349226e3d06ed1791875784500d
zachha <zachha@gmail.com>
2018-03-26 01:46:14 -0400

    create group function working

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,28 @@ function createUser(userName, password) {
     models.User.create({
         user_name: userName,
         password: password
-    }).then((data) => console.log(data.dataValues) );
-};
+    })
+    .then(data => console.log(data.dataValues))
+    .catch(err => console.log(err));
+}
 
-createUser("Trindy", "pword2");
+function updatePassword(userId, password) {
+    models.User.update({
+        password: password
+    }, {
+        where: {
+            id: userId
+        }})
+        // result gives back array with user id in it for some reason, look back at this later
+        .then(result => console.log(result))
+        .catch(err => console.log(err));
+}
+
+function createGroup(groupname) {
+    models.Groups.create({
+        group_name: groupname
+    });
+}
+
+//updatePassword(1, "NEWPASSSS");
+createGroup("NEWGROUP");
