08aac9f56761f32dfab65b312f847b5dd9dac695
HarryCaveMan <harris.joseph.088@gmail.com>
2018-03-28 16:16:27 -0400

    added jwt/passport authentication

---
+++ controllers/routes/user_api/auth.js
@@ -0,0 +1,33 @@
+route = require('express').Router();
+
+const express = require('express');
+const router  = express.Router();
+const jwt      = require('jsonwebtoken');
+const passport = require('passport');
+require('../../passport');
+/* POST login. */
+route.post('/', function (req, res, next) {
+    passport.authenticate('local', {session: false}, (err, user, info) => {
+        
+        if (err || !user) {
+          console.log('<>-----------------<>'+err+'<>-----------------<>');
+
+            return res.status(400).json({
+                message: info ? info.message : 'Login failed',
+                user   : user
+            });
+        }
+        req.login(user, {session: false}, (err) => {
+            if (err) {
+                res.send(err);
+            }
+            console.log("-------"+user.id+"-------")
+            const token = jwt.sign(user.id, 'secret');
+
+            return res.json({message:"okay",token: token});
+        });
+    })
+    (req, res);
+});
+
+module.exports = route;
