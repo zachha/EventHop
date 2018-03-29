f0c23e26cb94e5c32c62b4cac91c360541a6865d
HarryCaveMan <harris.joseph.088@gmail.com>
2018-03-29 13:22:44 -0400

    cleaning static files and implemented index.handlebars on '/' route

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // static directory
-app.use(express.static("public"));
+app.use('/public',express.static(path.join(__dirname+'/public')));
 
 // handlebars setup
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
