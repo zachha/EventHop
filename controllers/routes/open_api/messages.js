ab1d70d1eb233a2949a2c913503347d158f7b679
HarryCaveMan <harris.joseph.088@gmail.com>
2018-04-05 02:25:10 -0400

    Delete messages.js

--- controllers/routes/open_api/messages.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const route = express.Router();
-const mdb = require('../../messageController');
-
-route.get('/messages/:group/:n', (req,res,next) => {
-	mdb.findn(req.params.group,req.params.n).then(data => res.json(data.get{plain:true}));
-});
