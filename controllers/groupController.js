540b79294051ecc1073df184d67d560b1cfad1ed
zachha <zachha@gmail.com>
2018-06-20 19:39:03 -0400

    Update controller promises and seed info

--- controllers/groupController.js
+++ controllers/groupController.js
@@ -2,8 +2,9 @@ const models = require("../models");
 
 module.exports = {
   // Allows the user to create a group and then links the user to the group
-  createGroup: (groupname, userId, route, res) => {
+  createGroup: (groupname, groupMembers, userId, route, res) => {
     models.Groups.create({
+      group_members: groupMembers,
       group_name: groupname,
       route: route
     })
@@ -87,7 +88,9 @@ module.exports = {
       order: [["group_members", "DESC"]],
       limit: 5
       // again brings large array back, will have to select info we need here or server side
-    }).then(groups => {
+    })
+    .catch(err => console.log(err))
+    .then(groups => {
       let top5 = groups.map(group => {
         return { id: group.id, members: group.group_members };
       });
@@ -123,7 +126,7 @@ module.exports = {
 
   findAllGroups: () => {
     models.Groups.findAll({
-      order: [["group_members", "DESC"]],
+      order: [["id", "DESC"]],
       raw: true
     })
     .catch(err => console.log(err))
@@ -139,14 +142,14 @@ module.exports = {
       eventSpots: eventSpots,
       route: route
     })
-      .then(group =>
-        console.log(
-          group.get({
-            plain: true
-          })
-        )
+    .catch(err => console.log(err))
+    .then(group =>
+      console.log(
+        group.get({
+          plain: true
+        })
       )
-      .catch(err => console.log(err));
+    );
   },
 
   findAllEvents: () => {
@@ -155,24 +158,37 @@ module.exports = {
         is_event: true
       }
     })
+      .catch(err => console.log(err))
       .then(function(events) {
         console.log(events); //this info needs to be sifted through after it is sent back to the front end javascript
-      })
-      .catch(err => console.log(err));
+      });
+  },
+
+  deleteGroup: () => {
+    models.Groups.destroy({
+      where: {
+        is_event: true
+      }
+    })
+    .catch(err => console.log(err))
+    .then((events) => console.log("event deleted!"));
   }
 };
 
 /*
-module.exports.createGroup("Early Bird", 1,"Saladelia Cafe, 406 Blackwell St, Durham, NC 27701, USA&American Tobacco Campus, 318 Blackwell St, Durham, NC 27701&Bull City Burger and Brewery, 3318, 107 E Parrish St, Durham, NC 27701, USA");
-module.exports.createGroup("South By Southpoint", 1, "Firebirds Wood Fired Grill, 8030 Renaissance Pkwy, Durham, NC 27713&Barnes and Noble, 8030 Renaissance Pkwy, Durham, NC 27713&The Cheesecake Factory, 8030 Renaissance Pkwy, Durham, NC 27713");
-module.exports.createGroup("Nightlife Cabaret", 1, "Fullsteam Brewery, 726 Rigsbee Ave, Durham, NC 27701, USA&NanaSteak, 345 Blackwell St, Durham, NC 27701&West End Wine Bar of Durham, 601 W Main St, Durham, NC 27701, USA");
-module.exports.createGroup("Baseball and Brews", 1, "Brightleaf Square, 905 W Main St, Durham, NC 27701&Durham Bulls Athletic Park, 409 Blackwell St, Durham, NC 27701, USA&Tyler's Restaurant & Taproom, 324 Blackwell St, Durham, NC 27701, USA");
-module.exports.createGroup("One Night on Broadway", 1, "Bar Virgile, 105 S Mangum St, Durham, NC 27701&Durham Performing Arts Center, 123 Vivian Street, Durham, NC 27701&Luna Rotisserie and Empanadas, 112 W Main St, Durham, NC 27701");
+module.exports.createGroup("Early Bird", 1, 5, 11, "Saladelia Cafe, 406 Blackwell St, Durham, NC 27701, USA&American Tobacco Campus, 318 Blackwell St, Durham, NC 27701&Bull City Burger and Brewery, 3318, 107 E Parrish St, Durham, NC 27701, USA");
+module.exports.createGroup("South By Southpoint", 1, 6, 11, "Firebirds Wood Fired Grill, 8030 Renaissance Pkwy, Durham, NC 27713&Barnes and Noble, 8030 Renaissance Pkwy, Durham, NC 27713&The Cheesecake Factory, 8030 Renaissance Pkwy, Durham, NC 27713");
+module.exports.createGroup("Nightlife Cabaret", 1, 3, 11, "Fullsteam Brewery, 726 Rigsbee Ave, Durham, NC 27701, USA&NanaSteak, 345 Blackwell St, Durham, NC 27701&West End Wine Bar of Durham, 601 W Main St, Durham, NC 27701, USA");
+module.exports.createGroup("Baseball and Brews", 1, 10, 11, "Brightleaf Square, 905 W Main St, Durham, NC 27701&Durham Bulls Athletic Park, 409 Blackwell St, Durham, NC 27701, USA&Tyler's Restaurant & Taproom, 324 Blackwell St, Durham, NC 27701, USA");
+module.exports.createGroup("One Night on Broadway", 1, 4, 11, "Bar Virgile, 105 S Mangum St, Durham, NC 27701&Durham Performing Arts Center, 123 Vivian Street, Durham, NC 27701&Luna Rotisserie and Empanadas, 112 W Main St, Durham, NC 27701");
 
-module.exports.createGroup("BarHop", 10, "Bull City Burger and Brewery, 3318, 107 E Parrish St, Durham, NC 27701, USA&Motorco Parts and Labor, 723 Rigsbee Ave, Durham, NC 27701&Fullsteam Brewery, 726 Rigsbee Ave, Durham, NC 27701");
-module.exports.createGroup("Movie Date Night", 6, "Dos Perros, 200 N Mangum St, Durham, NC 27701&Carolina Theater, 309 W Morgan St, Durham, NC 27701&Pinhook, 117 W Main St, Durham, NC 27701");
-module.exports.createGroup("Spa Day", 7, "Posh the Salon, 610 W Main St #101, Durham, NC 27701&Pine Cone Ice Cream and Coffee, 905 W Main St, Durham, NC 27701&James Kennedy Galleries, 905 W Main St, Durham, NC 27701");
-module.exports.createGroup("Art and Coffee!", 8, "Pleiades Arts, 109 E Chapel Hill St, Durham, NC 27701&Vega Metals Inc, 214 Hunt Street, Durham, NC 27701, United States, Durham, NC 27701&Beyu Caffe, 341 W Main St, Durham, NC 27701");
-module.exports.createGroup("Brunch Crew", 11, "Parker and Otis, 112 S Duke St, Durham, NC 27701&Pine Cone Ice Cream and Coffee, 905 W Main St, Durham, NC 27701&The Retreat at Brightleaf, 2042, 815 W Morgan St, Durham, NC 27701");
+
+module.exports.createGroup("BarHop", 10, 11, "Bull City Burger and Brewery, 3318, 107 E Parrish St, Durham, NC 27701, USA&Motorco Parts and Labor, 723 Rigsbee Ave, Durham, NC 27701&Fullsteam Brewery, 726 Rigsbee Ave, Durham, NC 27701");
+module.exports.createGroup("Movie Date Night", 6, 11, "Dos Perros, 200 N Mangum St, Durham, NC 27701&Carolina Theater, 309 W Morgan St, Durham, NC 27701&Pinhook, 117 W Main St, Durham, NC 27701");
+module.exports.createGroup("Spa Day", 7, 11, "Posh the Salon, 610 W Main St #101, Durham, NC 27701&Pine Cone Ice Cream and Coffee, 905 W Main St, Durham, NC 27701&James Kennedy Galleries, 905 W Main St, Durham, NC 27701");
+module.exports.createGroup("Art and Coffee!", 8, 11, "Pleiades Arts, 109 E Chapel Hill St, Durham, NC 27701&Vega Metals Inc, 214 Hunt Street, Durham, NC 27701, United States, Durham, NC 27701&Beyu Caffe, 341 W Main St, Durham, NC 27701");
+module.exports.createGroup("Brunch Crew", 11, 11, "Parker and Otis, 112 S Duke St, Durham, NC 27701&Pine Cone Ice Cream and Coffee, 905 W Main St, Durham, NC 27701&The Retreat at Brightleaf, 2042, 815 W Morgan St, Durham, NC 27701");
 */
-module.exports.findAllGroups();
+
+//module.exports.deleteGroup();
+//module.exports.findAllGroups();
--- controllers/userController.js
+++ controllers/userController.js
@@ -8,8 +8,8 @@ module.exports = {
       user_name: userName,
       password: password
     })
-      .then(data => console.log(data.dataValues))
-      .catch(err => console.log(err));
+    .catch(err => console.log(err))
+    .then(data => console.log(data.dataValues));
   },
 
   // Allows user to change their password
@@ -24,11 +24,11 @@ module.exports = {
         }
       }
     )
+      .catch(err => console.log(err))
       // result gives back array with user id in it for some reason, look back at this later
       .then(result =>
         console.log("User: " + userId + "'s password was successfully changed!")
-      )
-      .catch(err => console.log(err));
+      );
   },
 
   // Allows user to find another user by their user name
@@ -43,17 +43,18 @@ module.exports = {
         }
       ]
     })
-      .then(user => user.get({ plain: true }))
-      .catch(err => console.log(err));
+      .catch(err => console.log(err))
+      .then(user => user.get({ plain: true }));
   },
 
   // Allows user to see all other users, in descending order by how many groups the users are in
   searchAllUsers: () => {
     models.User.findAll({
-      order: [["number_of_Groups", "DESC"]]
+      order: [["number_of_Groups", "DESC"]],
+      raw: true
     })
-      .then(users => console.log(users))
-      .catch(err => console.log(err));
+      .catch(err => console.log(err))
+      .then(users => console.log(users));
   }
 };
 
@@ -66,4 +67,4 @@ module.exports.createUser("john@email.com", "John", "john");
 module.exports.createUser("tom@email.com", "Tom", "tom");
 */
 
-module.exports.searchAllUsers();
+//module.exports.searchAllUsers();
